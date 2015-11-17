dfbbb287268b6ffedd0249815832fb28a6888e6e
Eivind Eidheim Elseth <eivind.elseth@comperio.no>
2015-11-17 21:23:05 +0100

    Added local storage and made page dynamic

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,4 @@
  */
 angular.module('yoApp')
 	.controller('MainCtrl', function () {
-
 	});
--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -8,6 +8,16 @@
  * Controller of the yoApp
  */
 angular.module('yoApp')
-  .controller('ProfileCtrl', function () {
-
-  });
+	.controller('ProfileCtrl', function ($scope, localStorageService) {
+		$scope.user = localStorageService.get('user') || {};
+		$scope.save = function () {
+			var user = {
+				'name' : $scope.user.name,
+				'email': $scope.user.email,
+				'tlf' : $scope.user.tlf,
+				'address': $scope.user.address,
+				'allowsMail': $scope.user.allowsMail
+			};
+			localStorageService.set('user', user);
+		};
+	});
