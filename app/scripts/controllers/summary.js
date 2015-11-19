31fb5c48c2a2453a278a5ea2787556de455a03ed
Eivind Eidheim Elseth <eivind.elseth@comperio.no>
2015-11-19 19:24:56 +0100

    Added check for missing data, and returned lists of missing fields.

--- app/scripts/controllers/summary.js
+++ app/scripts/controllers/summary.js
@@ -14,14 +14,110 @@ angular.module('yoApp')
 
 		var lessonsStorageName = 'lessons';
 		$scope.lessonStore = localStorageService.get(lessonsStorageName) || {};
-		$scope.lessonStore.instrument = JSON.parse($scope.lessonStore.instrument);
+		if ($scope.lessonStore.instrument) {
+			$scope.lessonStore.instrument = JSON.parse($scope.lessonStore.instrument);
+		}
 
 		var billingStorageName = 'billing';
 		$scope.billingStore = localStorageService.get(billingStorageName) || {};
 
 		$scope.totalCost = function (number, price) {
-			console.log(number);
-			console.log(price);
 			return number * price;
+		};
+
+		$scope.allDataPresent = function () {
+			console.log('userdata:' + $scope.userDataPresent());
+			console.log('billingdata:' + $scope.billingDataPresent());
+			console.log('lessondata:' + $scope.lessonDataPresent());
+			return (
+				$scope.userDataPresent() &&
+				$scope.billingDataPresent() &&
+				$scope.lessonDataPresent()
+			);
+		};
+		$scope.userDataPresent = function () {
+			return (
+				$scope.user &&
+				// Check that all the user fields are set
+				$scope.user.name &&
+				$scope.user.email &&
+				$scope.user.tlf &&
+				$scope.user.address
+			);
+		};
+
+		$scope.billingDataPresent = function () {
+			return (
+				$scope.billingStore &&
+
+				// Check that all the billing fields are set
+				$scope.billingStore.name &&
+				$scope.billingStore.cardType &&
+				$scope.billingStore.cardNumber
+			);
+		};
+
+		$scope.lessonDataPresent = function () {
+			return (
+				$scope.lessonStore &&
+
+				// Check that all the lesson fields are set
+				$scope.lessonStore.instrument &&
+				$scope.lessonStore.numberOfLessons
+			);
+		};
+
+		$scope.missingUserFields = function () {
+			var missingFields = [];
+
+			if (!$scope.user.name) {
+				missingFields.push('Username');
+			}
+
+			if (!$scope.user.email) {
+				missingFields.push('Email');
+			}
+
+			if (!$scope.user.tlf) {
+				missingFields.push('Phone number');
+			}
+
+			if (!$scope.user.address) {
+				missingFields.push('Address');
+			}
+
+			return missingFields;
+		};
+
+		$scope.missingBillingFields = function () {
+			var missingFields = [];
+
+			if (!$scope.billingStore.name) {
+				missingFields.push('Cardholder name');
+			}
+
+			if (!$scope.billingStore.cardType) {
+				missingFields.push('Card type');
+			}
+
+			if (!$scope.billingStore.cardNumber) {
+				missingFields.push('Card number');
+			}
+
+			return missingFields;
+		};
+
+		$scope.missingLessonFields = function () {
+			var missingFields = [];
+
+			if (!$scope.lessonStore.instrument) {
+				missingFields.push('Instrument');
+			}
+
+			if (!$scope.lessonStore.numberOfLessons) {
+				missingFields.push('Number of lessons');
+			}
+
+			return missingFields;
 		};
   });
